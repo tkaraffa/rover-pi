b31b533d37b2f9d5a4bf4e49fdd79ebf0a2242d1
Tom Karaffa <tom.karaffa@protonmail.com>
2021-06-13 17:28:30 -0700

    .

--- rover/Tutorial/circleDrive/rover.py
+++ rover/Tutorial/circleDrive/rover.py
@@ -55,7 +55,7 @@ class Rover:
 
     def get_dht_sensor(self):
         pin = os.getenv("ATMOSPHERESENSOR")
-        sensor = exec(f'adafruit_dht.DHT11(board.{pin}, use_pulseio=False)', None, globals()) 
+        exec(f'sensor = adafruit_dht.DHT11(board.{pin}, use_pulseio=False)', None, globals()) 
         return sensor
 
 
