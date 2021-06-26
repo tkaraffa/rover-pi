15c6c55db6f63d3b47278054fbaf13b943d11a96
Tom Karaffa <tom.karaffa@protonmail.com>
2021-06-26 13:39:42 -0700

    .

--- rover/uploader.py
+++ rover/uploader.py
@@ -99,7 +99,7 @@ class Uploader:
 
     def sheet_wrapper(function):
         "Setup necessary for Google Sheets"
-        def wrapper(self, **kwargs):
+        def wrapper(**kwargs):
             if self.sheet is None:
                 self.sheet = self.open_sheet()
             try:
