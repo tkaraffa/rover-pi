53d83d06fc1d8b298125c1c2e53098999aefba7b
Tom Karaffa <tom.karaffa@protonmail.com>
2021-06-26 12:58:03 -0700

    .

--- rover/uploader.py
+++ rover/uploader.py
@@ -92,7 +92,7 @@ class Uploader:
         "median"
         return statistics.median(array)
 
-    def sheet_wrapper(function):
+    def sheet_wrapper(function, **kwargs):
         def wrapper(self, **kwargs):
             if self.sheet is None:
                 self.sheet = self.open_sheet()
