4aee6b9536580301971e6d0203edc244a2e6c5e8
Tom Karaffa <tom.karaffa@protonmail.com>
2021-06-26 19:10:41 -0700

    .

--- server/server_test.py
+++ server/server_test.py
@@ -3,5 +3,6 @@ sys.path.append("/home/pi/rover-pi")
 from server import Server
 
 if __name__ == "__main__":
-    test_app = Server()
-    test_app.app.run(debug=True, host='0.0.0.0')
+    server = Server()
+    app = server.app
+    app.run(debug=True, host='0.0.0.0')
