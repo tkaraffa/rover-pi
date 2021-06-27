a40da97859e0c5774f676d440189429e1afbe738
Tom Karaffa <tom.karaffa@protonmail.com>
2021-06-26 23:17:07 -0700

    .

--- server/server.py
+++ server/server.py
@@ -11,6 +11,35 @@ class Server(Uploader):
     def __init__(self):
         super(Server, self).__init__()
 
+        self.app = Flask(__name__)
+        self.debug = True
+        self.host = Flask_Enums.HOST.value
+
+        self.d = self.download_data()
+
+        @self.app.route('/')
+        def index():
+            now = datetime.now().strftime("%Y%m%dT%H%M%S")
+            templateData = {
+                'title': 'HELLO!',
+                'time': now,
+            }
+            return render_template('index.html', **templateData)
+        
+        @self.app.route('/test')
+        def test():
+            return render_template('test.html')
+
+        @self.app.route('/hello/<name>')
+        def hello(name):
+            return render_template('page.html', name=name)
+
+        @self.app.route('/data')
+        def data():
+            templateData = {
+                'data': str(self.d),
+            }
+            return render_template('data.html', **templateData)
 
     def run(self):
         self.app.run(debug=self.debug, host=self.host)
--- server/server_test.py
+++ server/server_test.py
@@ -1,55 +1,7 @@
 import sys
 sys.path.append("/home/pi/rover-pi")
-from rover.uploader import Uploader
-from config.uploader_enums import Flask_Enums
-from flask import Flask, render_template
-from datetime import datetime
+from server import Server
 
 if __name__ == "__main__":
-    uploader = Uploader()
-
-    def download_data(Uploader):
-        data = Uploader.sheet.get_all_records()
-        aggs = {
-            "reading_timestamp": datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
-        }
-        for function in Uploader.calculation_functions:
-            f_name = function.__doc__
-            aggs[f_name] = {}
-            for column in Uploader.numeric_columns:
-                array = [float(row.get(column)) for row in data if row.get(column) not in Uploader.null_values]
-                aggs[f_name][column] = function(array)
-        return aggs
-
-
-    u = download_data(uploader)
-    print(u)
-    app = Flask(__name__)
-    debug = True
-    host = Flask_Enums.HOST.value
-
-    @app.route('/')
-    def index():
-        now = datetime.now().strftime("%Y%m%dT%H%M%S")
-        templateData = {
-            'title': 'HELLO!',
-            'time': now,
-        }
-        return render_template('index.html', **templateData)
-    
-    @app.route('/test')
-    def test():
-        return render_template('test.html')
-
-    @app.route('/hello/<name>')
-    def hello(name):
-        return render_template('page.html', name=name)
-
-    @app.route('/data')
-    def data():
-        templateData = {
-            'data': u,
-        }
-        return render_template('data.html', **templateData)
-
-    app.run(debug=debug, host=host)
+    server = Server()
+    server.run()
