4abdf5265a92a28f3b9f67832753f95e2b9d49d6
Tom Karaffa <tom.karaffa@protonmail.com>
2021-06-26 18:30:12 -0700

    .

--- server/server.py
+++ server/server.py
@@ -3,7 +3,7 @@ sys.path.append("/home/pi/rover-pi")
 # sys.path.append("/home/pi/rover-pi/rover")
 
 print(sys.path)
-from uploader import Uploader
+from rover.uploader import Uploader
 
 class Server(Uploader):
     pass
