383466d41e0ac1c2b94becbaa1c5bf371c2e6184
Tom Karaffa <tom.karaffa@protonmail.com>
2021-12-03 11:42:27 -0800

    add docstrings & typehints

--- rover/sensor.py
+++ rover/sensor.py
@@ -1,31 +1,78 @@
 import sys
+
 sys.path.append("/home/pi/rover-pi")
 
 from config.rover_enums import Pins
 
 
 class Sensor:
-    def __init__(self):
+    """Class for reading environmental sensor data.
+
+    Attributes
+    ----------
+    LightSensor: gpiozero.LightSensor
+        Light Sensor class for measuring light
+        from a photoreceptor.
+
+    DHTSensor: adafruit_dht.DHT11
+        DHT Sensor class for meaasuring humidity
+        and temperature from a DHT sensor.
+    """
+
+    def __init__(self) -> None:
         super(Sensor, self).__init__()
 
         # pins
         self.LightSensor = Pins.LIGHTSENSOR.value
         self.DHTSensor = Pins.ATMOSPHERESENSOR.value
 
-    def sense_light(self):
-        try:
-            return self.LightSensor.value
-        except:
-            return None
+    def sense_light(self) -> float:
+        """Obtains current light reading
+        from the light sensor.
 
-    def sense_humidity(self):
+        Returns
+        -------
+        light: float
+            The current light reading
+        """
+        return self.LightSensor.value
+
+    def sense_humidity(self) -> float:
+        """Obtains current humidity reading
+        from the DHT sensor.
+
+        Returns
+        -------
+        humidity: float
+            The current humidity reading
+
+        Raises
+        ------
+        RuntimeError
+            in the case of a checksum failure, or if
+            the reading returns insufficient data
+        """
         try:
             return self.DHTSensor.humidity
-        except:
+        except RuntimeError:
             return None
 
-    def sense_temperature(self):
+    def sense_temperature(self) -> float:
+        """Obtains current temperature reading
+        from the DHT sensor.
+
+        Returns
+        -------
+        temperature: float
+            The current temperature reading
+
+        Raises
+        ------
+        RuntimeError
+            in the case of a checksum failure, or if
+            the reading returns insufficient data
+        """
         try:
             return self.DHTSensor.temperature
-        except:
+        except RuntimeError:
             return None
--- rover/uploader.py
+++ rover/uploader.py
@@ -5,14 +5,51 @@ sys.path.append("/home/pi/rover-pi")
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import statistics
-from datetime import datetime
 from config.uploader_enums import Sheets_Enums
 import glob
 from pathlib import Path
 
 
 class Uploader:
-    def __init__(self):
+    """Class for interacting with Google Sheets
+
+    Attibutes
+    ---------
+    sheet_name: str
+        The name of the Google Sheet in which to record data
+
+    scope: list
+        The Google API scope
+
+    credentials_file: str
+        The file that holds the Google Service Account credentials
+
+    credentials
+        The actual Google Service Account credentials
+
+    sheet
+        The Google Sheet to operate on
+
+    columns: list
+        The headers of the Google Sheet file
+
+    calculation_functions: dict
+        The key/value pairs of metrics and their calculation funcionts
+
+    upload_frequency: int
+        The rate at which to upload data
+
+    null_values: list
+        Values to interpret as Null
+
+    non_data_columns: list
+        Columns for which to avoid calculating metrics
+
+    numeric_columns: list
+        Columns for which to calculate metrics
+    """
+
+    def __init__(self) -> None:
 
         super(Uploader, self).__init__()
 
@@ -46,7 +83,21 @@ class Uploader:
         ]
 
     @staticmethod
-    def find_file(default, filename=None):
+    def find_file(default, filename: str = None) -> str:
+        """Finds the file containing Google Service Account credentials
+
+        Parameters
+        ----------
+        default: str
+            The default filename to look for
+
+        filename: str
+            The provided filename to look for
+
+        Returns
+        -------
+        file: str
+            The path to the credentials file"""
         if filename is None:
             filename = default
         else:
@@ -59,23 +110,66 @@ class Uploader:
         return files[0]
 
     @staticmethod
-    def find_spreadsheet_name(spreadsheet_name=None):
+    def find_spreadsheet_name(spreadsheet_name: str = None) -> str:
+        """Finds the spreadsheet name. If not provided,
+        uses a default value
+
+        Parameters
+        ----------
+        spreadsheet_name: str, optional
+            The name of the spreadhseet to use
+
+        Returns
+        -------
+        spreadsheet_name: str
+        """
         if spreadsheet_name is None:
             spreadsheet_name = Sheets_Enums.SPREADSHEET_NAME.value
         return spreadsheet_name
 
     @staticmethod
-    def find_scope(scope=None):
+    def find_scope(scope: list = None) -> list:
+        """Finds the Google API scope. If not provided,
+        uses a default value
+
+        Parameters
+        ----------
+        scope: list
+            The scope to use
+
+        Returns
+        -------
+        scope: list
+        """
         if scope is None:
             scope = Sheets_Enums.DEFAULT_SCOPE.value
         return scope
 
-    def create_credentials(self):
+    def create_credentials(self) -> dict:
+        """Creates Service Account Credentials to use for
+        authentication
+
+        Returns
+        -------
+        credentials
+            The credentials to pass to gspread.authorize()
+        """
         return ServiceAccountCredentials.from_json_keyfile_name(
             self.credentials_file, self.scope
         )
 
     def open_sheet(self):
+        """Opens the Sheet object to write to
+
+        Returns
+        -------
+        sheet
+            The opened Google Sheet object
+
+        Raises
+        ------
+        Any exception if met
+        """
         try:
             gc = gspread.authorize(self.credentials)
             return gc.open(self.sheet_name).sheet1
@@ -83,8 +177,16 @@ class Uploader:
             print(str(ex))
             return None
 
-    def read_columns(self):
-        "Try to get columns from reading the sheet - if this returns None, use default values"
+    def read_columns(self) -> list:
+        """Tries to get columns from reading the Sheet object.
+        If none are found (i.e., the sheet is empty),
+        uses default values
+
+        Returns
+        -------
+        headers: list
+            The headers of the Sheet object
+        """
         headers = self.sheet.get("A1:AAA1")[0]
         if headers is None:
             headers = Sheets_Enums.DEFAULT_COLUMNS.value
@@ -92,30 +194,42 @@ class Uploader:
         return headers
 
     @staticmethod
-    def calculate_average(array):
-        """average"""
+    def calculate_average(array) -> float:
+        """Calculates the average of the provided array."""
         return statistics.mean(array)
 
     @staticmethod
-    def calculate_median(array):
-        """median"""
+    def calculate_median(array) -> float:
+        """Calculates the median of the provided array."""
         return statistics.median(array)
 
     @staticmethod
-    def calculate_mode(array):
-        """mode"""
+    def calculate_mode(array) -> float:
+        """Calculates the mode of the provided array."""
         return statistics.mode(array)
 
     @staticmethod
-    def calculate_stdev(array):
-        """standard_deviation"""
+    def calculate_stdev(array) -> float:
+        """Calculates the standard deviation of the provided array."""
         return statistics.stdev(array)
 
-    def check_sheet(self):
+    def check_sheet(self) -> None:
+        """Ensures the Sheet object is already open
+        before attempting any opeations"""
         if self.sheet is None:
             self.sheet = self.open_sheet()
 
-    def upload_data(self, data):
+    def upload_data(self, data: dict) -> None:
+        """Uploads data to the class's Sheet object
+
+        Parameters
+        ----------
+        data: dict
+            The JSON-formatted data
+
+        Raises
+        ------
+        Any exception if met, and resets the Sheet object"""
         self.check_sheet()
         try:
             row = [data.get(column) for column in self.columns]
@@ -124,7 +238,25 @@ class Uploader:
             print(str(e))
             self.sheet = None
 
-    def download_data(self, aggregations=None):
+    def download_data(self, aggregations: list = None) -> dict:
+        """Downloads data from the sheet for the provided aggregations.
+        If not provided, uses the default battery of aggregations
+        (mean, median, mode, std dev)
+
+        Parameters
+        ----------
+        aggregations: list, optional
+            The aggregations to perform
+
+        Returns
+        -------
+        aggregated_data: dict
+            The JSON-formatted aggregated data
+
+        Raises
+        ------
+            Any exception if met, and resets the Sheet object
+        """
         self.check_sheet()
         try:
             data = self.sheet.get_all_records()
@@ -152,7 +284,23 @@ class Uploader:
             print(str(e))
             self.sheet = None
 
-    def download_id_column_values(self, id_column="ID"):
+    def download_id_column_values(self, id_column: str = "ID") -> list:
+        """Downloads the ID column
+
+        Parameters
+        ----------
+        id_column: str, default="ID"
+            The name of the column that contains ID values
+
+        Returns
+        -------
+        ids: list
+            The list of ID values
+
+        Raises
+        ------
+            Any exception if met, and resets the Sheet object
+        """
         self.check_sheet()
         try:
             cell = self.sheet.find(id_column)
@@ -163,7 +311,18 @@ class Uploader:
             self.sheet = None
             return []
 
-    def download_most_recent_record(self):
+    def download_most_recent_record(self) -> dict:
+        """Downloads the most recent record from the sheet
+
+        Returns
+        -------
+        record: dict
+            The JSON representation of the most recent record.
+
+        Raises
+        ------
+            Any exception if met, and resets the Sheet object
+        """
         self.check_sheet()
         try:
             return self.sheet.get_all_records()[-1]
